0394bff7ff04acb877b3f53cb57f7ced1b38deac
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-18 10:38:10 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var no = 12345
+var nama = "ADI"
+var baligh = true
+var anak = null
+var saudara = ["shanti", "yudi", "yuda", "dani", "ica"]
+
+console.log(no)
+console.log(nama)
+console.log(baligh)
+console.log(anak)
+console.log(saudara)
